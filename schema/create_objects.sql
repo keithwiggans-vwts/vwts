c6db8338f0dc8ad744c9e0f7f49fa9099ade2291
Keith Wiggans <keithwiggans@vwts.org>
2024-06-25 16:06:55 -0500

    Added tables related to waste stream codes devined by the EWC.

--- schema/create_objects.sql
+++ schema/create_objects.sql
@@ -59,7 +59,7 @@ CREATE TABLE donation (
 );
 
 DROP TABLE IF EXISTS secure.contact;
-CREATE TABLE pii.contact (
+CREATE TABLE secure.contact (
   contact_id SERIAL PRIMARY KEY,
   entity_id INTEGER REFERENCES entity(entity_id),
   contact VARCHAR(255) NULL,
@@ -80,7 +80,7 @@ CREATE TABLE pii.contact (
 );
 
 DROP TABLE IF EXISTS secure.order;
-CREATE TABLE pii.order (
+CREATE TABLE secure.order (
   order_id SERIAL PRIMARY KEY,
   entity_id INTEGER REFERENCES entity(entity_id),
   subscription_id INTEGER REFERENCES subscription(subscription_id),
@@ -94,22 +94,55 @@ CREATE TABLE pii.order (
   payment_token VARCHAR(255),  -- Optional, for storing tokenized payment information
 );
 
-
-CREATE TABLE waste_stream (
-  waste_stream_id SERIAL PRIMARY KEY,
-  business_id INTEGER NOT NULL REFERENCES business(business_id),
-  waste_type VARCHAR(255) NOT NULL,
-  annual_quantity INTEGER NOT NULL,
-  units VARCHAR(255), -- Can be weight (lbs, tons), volume (gallons, cubic yards), etc.
-  collection_frequency VARCHAR(255), -- Daily, Weekly, Bi-weekly, etc. 
-  special_handling_instructions TEXT
+-- Liters, Cubic Meters, Etc...
+DROP TABLE IF EXISTS quantity_type;
+CREATE TABLE quantity_type (
+  quantity_type_id SERIAL PRIMARY KEY,
+  quantity_type VARCHAR(255)
 );
 
--- Optional table for associating waste streams with specific materials
+-- Daily, Weekly, Monthly, Etc...
+DROP TABLE IF EXISTS collection_frequency;
+CREATE TABLE collection_frequency (
+  collection_frequency_id SERIAL PRIMARY KEY,
+  collection_frequency VARCHAR(255)
+);
 
-CREATE TABLE waste_stream_material (
+-- AN, AH, MH, MN, Etc...
+DROP TABLE IF EXISTS entry_type;
+CREATE TABLE collection_frequency (
+  entry_type_id VARCHAR(2) PRIMARY KEY,
+  entry_type VARCHAR(255)
+);
+
+-- descriptive example of this waste type
+DROP TABLE IF EXISTS waste_example;
+CREATE TABLE waste_example (
+  waste_example_id INTEGER NOT NULL PRIMARY KEY,
+  waste_example VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS entity_waste_stream;
+CREATE TABLE entity_waste_stream (
+  entity_id INTEGER NOT NULL REFERENCES entity(entity_id),
   waste_stream_id INTEGER NOT NULL REFERENCES waste_stream(waste_stream_id),
-  material_name VARCHAR(255) NOT NULL,
-  percentage INTEGER NOT NULL,
-  PRIMARY KEY (waste_stream_id, material_name)
+  quantity INTEGER,
+  quantity_type_id INTEGER NOT NULL REFERENCES quantity_type(quantity_type_id),
+  collection_frequency_id INTEGER NOT NULL REFERENCES collection_frequency(collection_frequency_id),
+  PRIMARY KEY (entity_id, waste_stream_id)
+);
+
+-- https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1021051/Waste_classification_technical_guidance_WM3.pdf
+-- Page 43
+DROP TABLE IF EXISTS waste_stream;
+CREATE TABLE waste_stream (
+  waste_stream_id INTEGER NOT NULL,
+  waste_stream_parent_id INTEGER,
+  waste_level INTEGER NOT NULL, -- 0, 1, 2
+  waste_stream_code VARCHAR(255) NOT NULL, -- 01, 02, ..., 99
+  waste_stream VARCHAR(255) NOT NULL,
+  entry_type_id VARCHAR(2) NOT NULL REFERENCES entry_type(entry_type_id),
+  waste_example_id INTEGER REFERENCES waste_example(waste_example_id),
+  is_hazardous BOOLEAN,
+  PRIMARY KEY (waste_stream_id, waste_stream_parent_id, waste_level)
 );
